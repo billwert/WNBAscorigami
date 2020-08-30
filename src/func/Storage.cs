a6f5e53bd4e52a00d1b92e7989f4dc6cf4ef7aa9
Bill Wert <billwert@microsoft.com>
2020-08-30 13:01:06 -0700

    scraper azure function (#20)
    * scraper azure function
    
    * fix some white space
    
    * extract Storage code

---
+++ src/func/Storage.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace WNBAScorigami
+{
+    class Storage
+    {
+        private static string blobConnectionString;
+        private static BlobServiceClient serviceClient;
+        private static BlobContainerClient containerClient;
+
+        public Storage(string secret, string container)
+        {
+            blobConnectionString = Environment.GetEnvironmentVariable(secret);
+            serviceClient = new BlobServiceClient(blobConnectionString);
+            containerClient = serviceClient.GetBlobContainerClient(container);
+        }
+
+        public async Task UploadJson(string json, string name)
+        {
+            var blob = containerClient.GetBlobClient(name);
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            await blob.UploadAsync(stream, new BlobHttpHeaders
+            {
+                ContentType = "application/json"
+            });
+        }
+
+        public async Task<string> DownloadJson(string name)
+        {
+            var blob = containerClient.GetBlobClient(name);
+            BlobDownloadInfo jsonBytes = await blob.DownloadAsync();
+            using var ms = new MemoryStream();
+            await jsonBytes.Content.CopyToAsync(ms);
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+    }
+}
